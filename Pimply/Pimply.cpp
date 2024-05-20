5adf22c512c997c1722d318359d97f3e23638bcc
Andrei_Gridin <andreigridin918@gmail.com>
2024-05-21 02:13:39 +0500

    Добавьте файлы проекта.

---
+++ Pimply/Pimply.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void do_pimply(bool(*p)[12])
+{
+	for (int i = 0; i < 12; i++)
+	{
+		for (int j = 0; j < 12; j++)
+		{
+			p[i][j] = true;
+		}
+	}
+}
+
+int main()
+{
+    bool pimply[12][12];
+	do_pimply(pimply);
+
+
+
+}
